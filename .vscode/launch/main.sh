9d7703e1920540d27a4c0dbbddaad9e16799f091
vike <vike2000@gmail.com>
2021-12-19 03:23:49 +0100

    working personal debug process in vscode (cppdbg & lldb)

--- .vscode/launch/main.sh
+++ .vscode/launch/main.sh
@@ -2,6 +2,7 @@
 C=(
 	vphp3.8
 	-- -dextension=./modules/ui.so
-	-- -Prexc -\(,new\ UI\\Window -\( "test" -,new\ UI\\Size'(640,480)' -\)\)' -> 'show\(\) -\; UI\\run -\(\)
+#	-- -Prexc -\(,new\ UI\\Window -\( "test" -,new\ UI\\Size'(640,480)' -\)\)' -> 'show\(\) -\; UI\\run -\(\)
+	-- -Prexc -r\ui -\(d\ui\\window -\(,new\ UI\\Size'(640,480)' -\)\)' -> 'add -\(,new\ UI\\Area -\(  -\)\)' -> 'showAndRun\(\)
 #	-v
 	)
