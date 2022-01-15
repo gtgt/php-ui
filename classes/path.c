69950071ab5ed48d243abb6a44bebb56b4cbd586
vike <vike2000@gmail.com>
2022-01-15 20:42:42 +0100

    fixing Path::transform() (incl. libui rename)

--- classes/path.c
+++ classes/path.c
@@ -266,7 +266,7 @@ PHP_METHOD(DrawPath, isEnded)
 } /* }}} */
 #endif
 
-#ifdef LIBUI_HAS_DRAW_PATH_TRANSFORM
+#ifdef LIBUI_HAS_DRAW_PATH_COPY_BY_TRANSFORM
 PHP_UI_ZEND_BEGIN_ARG_WITH_RETURN_OBJECT_INFO_EX(php_ui_path_transform_info, 0, 1, UI\\Draw\\Path, 0)
 	ZEND_ARG_OBJ_INFO(0, matrix, UI\\Draw\\Matrix, 0)
 ZEND_END_ARG_INFO()
@@ -274,19 +274,22 @@ ZEND_END_ARG_INFO()
 /* {{{ proto Path UI\Draw\Path::transform(Matrix matrix) */
 PHP_METHOD(DrawPath, transform)
 {
-	php_ui_path_t *path = php_ui_path_fetch(getThis());
-	php_ui_path_t *result;
-	php_ui_matrix_t *matrix;
+	php_ui_path_t *p = php_ui_path_fetch(getThis());
+	php_ui_path_t *r;
+	zval *matrix = NULL;
+	php_ui_matrix_t *m;
 
 	if (zend_parse_parameters_throw(ZEND_NUM_ARGS(), "O", &matrix, uiDrawMatrix_ce) != SUCCESS) {
 		return;
 	}
 
+	m = php_ui_matrix_fetch(matrix);
+
 	object_init_ex(return_value, uiDrawPath_ce);
 
-	result = php_ui_path_fetch(return_value);
+	r = php_ui_path_fetch(return_value);
 
-	result->p = uiDrawCopyPathByTransform(path->p, &matrix->m);
+	r->p = uiDrawPathCopyByTransform(p->p, &m->m);
 } /* }}} */
 #endif
 
@@ -304,7 +307,7 @@ const zend_function_entry php_ui_path_methods[] = {
 	#ifdef LIBUI_HAS_DRAW_PATH_ENDED
 	PHP_ME(DrawPath, isEnded, php_ui_path_is_ended_info, ZEND_ACC_PUBLIC)
 	#endif
-	#ifdef LIBUI_HAS_DRAW_PATH_TRANSFORM
+	#ifdef LIBUI_HAS_DRAW_PATH_COPY_BY_TRANSFORM
 	PHP_ME(DrawPath, transform, php_ui_path_transform_info, ZEND_ACC_PUBLIC)
 	#endif
 	PHP_FE_END
