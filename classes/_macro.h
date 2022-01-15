2a367961df867cb6c6800d086408d968f4af8090
vike <vike2000@gmail.com>
2022-01-15 19:53:14 +0100

    adding macro PHP_UI_ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX

--- classes/_macro.h
+++ classes/_macro.h
@@ -10,4 +10,12 @@
 	ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(name, return_reference, required_num_args, IS_OBJECT, #class_name, allow_null)
 #endif
 
+#if PHP_VERSION_ID >= 70200
+#define PHP_UI_ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(name, return_reference, required_num_args, type, allow_null)\
+	ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(name, return_reference, required_num_args, type, allow_null)
+#else
+#define PHP_UI_ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(name, return_reference, required_num_args, type, allow_null)\
+	ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(name, return_reference, required_num_args, type, NULL, allow_null)
+#endif
+
 #endif
